148c27bb39de27d2c689b8a34a5f21553d78c806
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-09-07 15:48:41 -0700

    add logo

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -63,7 +63,7 @@ auth.onAuthStateChanged(handleAuthStateChanged);
         <div class="flex h-16 items-center justify-between">
           <div class="flex items-center">
             <div class="flex-shrink-0">
-              <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
+              <img class="h-8 w-8" src="/laforallwhite.png" alt="Los Angeles for All">
             </div>
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
