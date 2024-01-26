a12d60b68b2fbeccb4c7c39a8265e49c64dd7f28
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-01-26 13:59:51 -0800

    link to login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -51,8 +51,8 @@
         <h1 class='text-3xl font-bold text-white resetfilter'>Find Your Place to Make Change.</h1>  
         <p>Explore and connect with hundreds of local unions, collectives, and non-profits.</p>
         <div class='flex flex-row gap-x-3 mt-2'>
-        <button class='bg-white text-black px-3 py-2'>Login</button>
-        <button class='bg-orange-400 px-3 py-2'>Org Signup</button>
+        <a href='/login'><button class='bg-white text-black px-3 py-2'>Login</button></a>
+        <a href='/login'></a><button class='bg-orange-400 px-3 py-2'>Org Signup</button></a>
         </div>
     </div>
 </div>
